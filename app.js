a4155e32546f94d11ade312db6f4bc93d25e96ae
Kartikeya <kartikeya.knightriders@gmail.com>
2020-03-16 02:08:03 +0530

    Level 1 of authentication. Username and Password

---
+++ app.js
@@ -0,0 +1,83 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const  app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+// Connecting to DB
+
+mongoose.connect('mongodb://localhost/userDB', {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User",userSchema);
+
+
+
+
+
+
+
+
+// Routes
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    user.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password  === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000...");
+})
