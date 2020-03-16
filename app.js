cd44c8e553d6d341cc078db4d7e5113321dfdd8e
Kartikeya <kartikeya.knightriders@gmail.com>
2020-03-16 18:24:22 +0530

    Level 4 :- Encryption and Salting using bcrypt.

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-
+const  bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 const  app = express();
@@ -48,31 +47,35 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register", function(req,res){
-    const user = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    user.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const user = new User({
+            email: req.body.username,
+            password: hash
+        });
+        user.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
+    });    
 });
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(err,foundUser){
         if(err){
             console.log(err);
         }else{
             if(foundUser){
-                if(foundUser.password  === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
